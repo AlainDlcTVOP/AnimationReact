1428c7f191080459742881b71f166c2d3bfdfef8
AlainDlcTVOP <alain_dlc@outlook.com>
2021-06-22 20:34:28 +0200

    Interpolation

--- swipe/App.js
+++ swipe/App.js
@@ -3,10 +3,11 @@ import { Card, Button } from 'react-native-elements';
 import { StyleSheet, Text, View } from 'react-native';
 import Deck from './src/Deck';
 
+
 const DATA = [
   { id: 1, text: 'Card #1', uri: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Kawasaki_Ninja_250_2018.jpg' },
   { id: 2, text: 'Card #2', uri: 'http://www.kevamotor.se/wp-content/uploads/2014/09/kx-250-14.jpg' },
-  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 3, text: 'Card #3', uri: '../assets/img.jpg' },
   { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
   { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
   { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
 
     constructor(props) {
         super(props);
-
+        const position = new Animated.ValueXY();
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
-
-
+                position.setValue({ x: gesture.dx, y: gesture.dy });
             },
             onPanResponderRelease: () => { }
         });
-        this.state = { panResponder }
+        this.state = { panResponder, position };
+    }
+
+    getCardStyle() { //helper method
+        // setUp  interpolate
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]  // rotate the image
+        };
     }
 
     renderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
+
             return this.props.renderCard(item);
         });
     }
@@ -27,7 +53,7 @@ class Deck extends Component {
     render() {
 
         return (
-            <View {...this.state.panResponder.panHandlers}>
+            <View>
                 {this.renderCards()}
             </View>
 
